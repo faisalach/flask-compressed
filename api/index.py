3bf9243f0bcd79e479e0a9e46aea7a7b09a4205d
faisalach <faisalachramsyah@gmail.com>
2024-06-21 16:30:27 +0700

    fix

--- api/index.py
+++ api/index.py
@@ -4,19 +4,6 @@ import random
 from flask import Flask, request,jsonify,send_from_directory
 from werkzeug.utils import secure_filename
 
-from audio.mp3 import compress_audio_mp3
-from audio.ogg import compress_audio_ogg
-from gambar.webp import compress_gambar_webp
-from gambar.jpeg import compress_gambar_jpeg
-from video.h264 import compress_video_h264
-from video.h265 import compress_video_h265
-
-UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'wav', 'mp4', 'jpg'}
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
